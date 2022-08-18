c76e5c7b2f64f70fd4d7d408853c0167ed7408fb
ShantoMRozario <shantoxrozario@gmail.com>
2022-08-18 15:37:18 +0600

    added appon files

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Feahter Icon 
+feather.replace();
+
+// Banner Slider
+$('.ban_slide_item').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    arrows:false,
+    infinite:true,
+    autoplay:true,
+  });
+
+//Video
+new VenoBox({
+  selector: '.appon_video',
+  maxWidth: "70%",
+}); 
+
+// Feedback Slider
+
+
